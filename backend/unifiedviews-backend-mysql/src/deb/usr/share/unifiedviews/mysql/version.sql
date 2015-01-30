e2ae498b8318e2cdeaf0eb51fe4a123d412b38b2
Jan Marcek <jan.marcek@eea.sk>
2015-01-30 10:42:51 +0100

    #14  unifiedviews-backend-mysql - set db version is removed. Use UV.Core.version in table properties for that

--- backend/unifiedviews-backend-mysql/src/deb/usr/share/unifiedviews/mysql/version.sql
+++
@@ -1,8 +0,0 @@
--- set db schema version into database 
-LOCK TABLES `properties` WRITE;
-/*!40000 ALTER TABLE `properties` DISABLE KEYS */;
-INSERT INTO `properties` VALUES ('UV.db.version','001.005.000');
-/*!40000 ALTER TABLE `properties` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
