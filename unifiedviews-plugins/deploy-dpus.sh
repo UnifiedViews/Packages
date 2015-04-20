ce92dc6b95ba14269e6d9996f5a345c8e7f1e04f
Peter Klimo <peter.klimo@eea.sk>
2015-04-20 09:59:53 +0200

    port, username and password changed

--- unifiedviews-plugins/deploy-dpus.sh
+++ unifiedviews-plugins/deploy-dpus.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-URL="http://127.0.0.1:18080/master/api/1/import/dpu/jar"
-MASTER_USER=user
-MASTER_PASS=pass
+URL="http://127.0.0.1:28080/master/api/1/import/dpu/jar"
+MASTER_USER=master
+MASTER_PASS=commander
 
 echo "---------------------------------------------------------------------"
 echo "Installing DPUs.."
 echo "..target instance: $URL"
 echo "---------------------------------------------------------------------"
 
-function install_dpu() {
+install_dpu() {
     dpu_file=$(ls $1)
 
     echo -n "..installing $dpu_file: "
