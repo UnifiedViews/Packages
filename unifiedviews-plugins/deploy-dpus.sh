e8e1a640bae6560969318b2da8a395ca1200d680
Peter Klimo <peter.klimo@eea.sk>
2015-04-20 11:46:03 +0200

    typo + paths added

--- unifiedviews-plugins/deploy-dpus.sh
+++ unifiedviews-plugins/deploy-dpus.sh
@@ -10,16 +10,13 @@ echo "..target instance: $URL"
 echo "---------------------------------------------------------------------"
 
 install_dpu() {
-
-install_dpu() {
-    dpu_path=/usr/share/unifiedviews/dist/plugins/
     dpu_file=$(ls $1)
 
     echo -n "..installing $dpu_file: "
     outputfile="/tmp/$dpu_file.out"
 
     # fire cURL and wait until it finishes
-    curl --user $MASTER_USER:$MASTER_PASS --fail --silent --output $outputfile -X POST -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F file=@$dpu_path$dpu_file $URL?force=true 
+    curl --user $MASTER_USER:$MASTER_PASS --fail --silent --output $outputfile -X POST -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F file=@$dpu_file $URL?force=true 
     wait $!
 
     # check if the installation went well
@@ -27,25 +24,25 @@ install_dpu() {
     echo $outcontents
 }
 
-install_dpu "uv-e-filesDownload-*.jar”
-install_dpu "uv-e-relationalFromSql-*.jar”
-install_dpu "uv-l-filesToVirtuoso-*.jar”
-install_dpu "uv-l-filesUpload-*.jar”
-install_dpu "uv-l-relationalToSql-*.jar”
-install_dpu "uv-t-filesFilter-*.jar”
-install_dpu "uv-t-filesMerger-*.jar”
-install_dpu "uv-t-filesRenamer-*.jar”
-install_dpu "uv-t-filesToRdf-*.jar”
-install_dpu "uv-t-filterValidXml-*.jar”
-install_dpu "uv-t-metadata-*.jar”
-install_dpu "uv-t-rdfMerger-*.jar”
-install_dpu "uv-t-rdfToFiles-*.jar”
-install_dpu "uv-t-relational-*.jar”
-install_dpu "uv-t-sparqlConstruct-*.jar”
-install_dpu "uv-t-sparqlSelect-*.jar”
-install_dpu "uv-t-sparqlUpdate-*.jar”
-install_dpu "uv-t-tabular-*.jar”
-install_dpu "uv-t-tabularToRelational-*.jar”
-install_dpu "uv-t-unzipper-*.jar”
-install_dpu "uv-t-xslt-*.jar”
-install_dpu "uv-t-zipper-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-e-filesDownload-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-e-relationalFromSql-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-l-filesToVirtuoso-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-l-filesUpload-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-l-relationalToSql-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-filesFilter-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-filesMerger-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-filesRenamer-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-filesToRdf-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-filterValidXml-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-metadata-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-rdfMerger-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-rdfToFiles-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-relational-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-sparqlConstruct-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-sparqlSelect-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-sparqlUpdate-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-tabular-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-tabularToRelational-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-unzipper-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-xslt-*.jar”
+install_dpu "/usr/share/unifiedviews/dist/plugins/uv-t-zipper-*.jar”
